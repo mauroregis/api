0b96a10d50d05c1a34d5205f0d9ef96e35b2b586
Mauro Regis <mauroregis@gmail.com>
2024-04-30 09:38:21 -0300

    v0

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var host = HttpContext.Request.Host.Value;
+
+            return Ok(host);
+        }
+    }
+}
