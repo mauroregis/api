d9c8a59fd88e37d92032e0c22f2d3e4cb3376b7b
Mauro Regis <mauroregis@gmail.com>
2024-05-10 18:48:22 -0300

    teste github

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
             {
                 message = "Healthy",
                 time = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"),
-                version = "1.0.2"
+                version = "1.0.3"
             });
 
             return Ok(json);
